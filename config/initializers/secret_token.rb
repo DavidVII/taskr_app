967170dccd291e10b30faf30834030947ed601c2
David Becerra <David@redeyedesigner.com>
2013-05-11 01:34:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaskrApp::Application.config.secret_key_base = 'c13301fbceb35010f8b3aaef8c00fb086a4e92995baeaf2df8f6684adeb5afb6bfe1c1fb6f0b0c4d5506e9d1ca0e5b56f56b8411b03e53425e3db55c28b209e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TaskrApp::Application.config.session_store :cookie_store, key: '_taskr_app_session'
