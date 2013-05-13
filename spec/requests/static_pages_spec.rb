bf8336f409b49a04da3dd2731e82f92de69be7a8
David Becerra <David@redeyedesigner.com>
2013-05-12 21:33:32 -0700

    DRY up static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,44 +1,28 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  subject { page }
+
   describe "Home Page" do
-    it "should have the content 'Taskr'" do
-      visit root_path
-      page.should have_selector('h1', text: "Taskr")
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title', text: "Taskr App")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title', text: "| Home")
-    end
+    it { should have_selector('h1', text: "Taskr") }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
   end
 
   describe "About Page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', text: "About Us")
-    end
+    before { visit about_path }
 
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title', text: "Taskr App | About")
-    end
+    it { should have_selector('h1', text: "About Us") }
+    it { should have_selector('title', text: full_title("About")) }
   end
 
   describe "Contact Page" do
-    it "should have the content 'Contact Us'" do
-      visit contact_path
-      page.should have_selector('h1', text: "Contact Us")
-    end
+    before { visit contact_path }
 
-    it "should have the right title" do
-      visit contact_path
-      page.should have_selector('title', text: "Taskr App | Contact")
-    end
+    it { should have_selector('h1', text: "Contact Us") }
+    it { should have_selector('title', text: full_title("Contact")) }
   end
 end
